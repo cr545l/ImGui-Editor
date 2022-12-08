489bcb3b5e6b48377d5ed49825aafc4c6e37056f
cr545l <cr545l@gmail.com>
2022-12-08 19:09:58 +0900

    Widget, Editor code split

--- src/Editor/widget/inspector.cpp
+++ src/Editor/widget/inspector.cpp
@@ -5,7 +5,6 @@
 #include "editor/widget.h"
 #include "editor/widget/args_data.h"
 #include "editor/selection.h"
-#include "editor/widget_editor.h"
 
 extern ImVec2 g_unitSize;
 
@@ -48,55 +47,6 @@ static const ImGuiStyleVarInfo GStyleVarInfo[] =
 
 namespace imgui_editor
 {
-	void draw_widget_inspector(widget_inspector* context)
-	{
-		auto selected = selection::get_targets();
-		ImGui::Text("Selected %lu", selected.size());
-
-		if(selected.size())
-		{
-			widget* ctx = selection::get_targets()[0];
-
-			ImGui::Separator();
-			
-			if(ImGui::TreeNode("Gen c++"))
-			{
-				std::string s =  widget_generate(generate_code::cpp, ctx);
-				ImGui::InputTextMultiline("##data", &s, ImVec2(0, g_unitSize.y*50), ImGuiInputTextFlags_ReadOnly);
-				ImGui::TreePop();
-
-                if(ImGui::Button("Copy"))
-                {
-                    ImGui::SetClipboardText(s.c_str());
-                }
-			}
-			
-			if(ImGui::TreeNode("Output"))
-			{
-				std::string s =  widget_serialize(ctx);
-				ImGui::InputTextMultiline("##data", &s, ImVec2(0, g_unitSize.y*2), ImGuiInputTextFlags_ReadOnly);
-				ImGui::TreePop();
-			}
-
-			if(ImGui::TreeNode("Input"))
-			{
-				ImGui::InputTextMultiline("##data", &context->input, ImVec2(0, g_unitSize.y*2));
-				if(ImGui::Button("Apply"))
-				{
-					widget_deserialize(ctx, context->input.c_str());
-				}
-
-				ImGui::TreePop();
-			}
-
-			ImGui::Separator();
-
-			for (auto i : selected)
-			{
-				draw_inspector_widget(i);
-			}
-		}
-	}
 
     void draw_inspector_widget(widget *ctx)
     {
---
+++ src/Editor/window/inspector.cpp
@@ -0,0 +1,59 @@
+#include "Precompiled.h"
+#include "editor/widget.h"
+#include "editor/widget_editor.h"
+#include "editor/selection.h"
+
+extern ImVec2 g_unitSize;
+
+namespace imgui_editor
+{
+	void draw_widget_inspector(widget_inspector* context)
+	{
+		auto selected = selection::get_targets();
+		ImGui::Text("Selected %lu", selected.size());
+
+		if(selected.size())
+		{
+			widget* ctx = selection::get_targets()[0];
+
+			ImGui::Separator();
+			
+			if(ImGui::TreeNode("Gen c++"))
+			{
+				std::string s =  widget_generate(generate_code::cpp, ctx);
+				ImGui::InputTextMultiline("##data", &s, ImVec2(0, g_unitSize.y*50), ImGuiInputTextFlags_ReadOnly);
+				ImGui::TreePop();
+
+                if(ImGui::Button("Copy"))
+                {
+                    ImGui::SetClipboardText(s.c_str());
+                }
+			}
+			
+			if(ImGui::TreeNode("Output"))
+			{
+				std::string s =  widget_serialize(ctx);
+				ImGui::InputTextMultiline("##data", &s, ImVec2(0, g_unitSize.y*2), ImGuiInputTextFlags_ReadOnly);
+				ImGui::TreePop();
+			}
+
+			if(ImGui::TreeNode("Input"))
+			{
+				ImGui::InputTextMultiline("##data", &context->input, ImVec2(0, g_unitSize.y*2));
+				if(ImGui::Button("Apply"))
+				{
+					widget_deserialize(ctx, context->input.c_str());
+				}
+
+				ImGui::TreePop();
+			}
+
+			ImGui::Separator();
+
+			for (auto i : selected)
+			{
+				draw_inspector_widget(i);
+			}
+		}
+	}
+}
