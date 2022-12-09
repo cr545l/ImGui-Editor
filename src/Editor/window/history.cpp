0418aa867d8eeec9a731dbb296b07f0cc45cde21
cr545l <cr545l@gmail.com>
2022-12-09 20:21:39 +0900

    Add Auto-scroll

--- src/Editor/window/history.cpp
+++ src/Editor/window/history.cpp
@@ -6,27 +6,32 @@ extern ImVec2 g_unitSize;
 
 namespace imgui_editor
 {
+    static size_t last_index = 0;
     void draw_histroy(history* ctx)
     {
-        if (ImGui::BeginChild("add widget", ImVec2(0.f, g_unitSize.y * 25), true))
+        static bool auto_scroll = true;
+        
+        bool changed = last_index != ctx->index;
+        last_index = ctx->index;
+        ImGui::Checkbox("Auto-scroll", &auto_scroll);
+        if(ImGui::BeginListBox("History"))
         {
-            ImGui::Text("History");
+            for(size_t i = 0; i < ctx->commands.size(); ++i)
+            {           
+                const bool is_selected = i == ctx->index;
 
-            if(ImGui::BeginListBox("History"))
-            {
-                for(size_t i = 0; i < ctx->commands.size(); ++i)
+                if(ImGui::Selectable(ctx->commands[i]->label.c_str(), is_selected, ImGuiSelectableFlags_SelectOnNav))
                 {
-                    if(ImGui::Selectable(ctx->commands[i]->label.c_str(), i == ctx->index))
-                    {
-                        ctx->index = i;
-                    }
+                    ctx->index = i;
                 }
-
-                ImGui::EndListBox();
+                if (auto_scroll && is_selected && changed && 0<ImGui::GetScrollMaxY()) 
+                {
+                    ImGui::SetScrollHereY(ImGui::GetScrollY()/ImGui::GetScrollMaxY());
+                }
             }
+            ImGui::EndListBox();
         }
-        ImGui::EndChild();
-
+        ImGui::Separator();
         ImGui::Dummy(ImVec2(0.f, g_unitSize.y));
     }
 }
