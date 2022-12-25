5fe06b5d7788fbe618a2294a4c36f6873a7ea441
cr545l <cr545l@gmail.com>
2022-12-25 11:59:10 +0900

    Move message

--- src/Editor/widget/args_data.h
+++ src/Editor/widget/args_data.h
@@ -3,6 +3,28 @@
 
 #pragma pack(push, 1)
 
+//
+// # Logic required by the widget
+// 
+// - Args data.h
+// - Delete.cpp
+// - Deserialize.cpp
+// - Draw.cpp
+// - Enum type.h
+// - Fixed type.cpp
+// - Gen code.cpp
+// - Inspector.cpp
+// - New.cpp
+// - Pretty name.cpp
+// - To enum type.cpp
+//
+// # Files affected when the `args_data.h` struct field is changed
+// 
+// - Deserialize.cpp
+// - Draw.cpp
+// - Gen code.cpp
+// - Inspector.cpp
+//
 namespace imgui_editor
 {	
 	struct widget_none {};
